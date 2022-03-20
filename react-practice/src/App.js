29e91be40b6e5a6cd13a37fdcb1cd9a2cf9190f0
Nasratredi <nasratredip@gmail.com>
2022-03-20 16:18:41 +0500

    React Components

---
+++ react-practice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./components/Product";
+
+function App() {
+  return (
+    <div>
+      <h1>Component Example</h1>
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practice/src/components/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const product = () => {
+  var title =  "My Products"
+  return <div>
+    <h1>Product Title</h1>
+    <p>Product Descripttion</p>
+  </div>;
+};
+
+export default product;
